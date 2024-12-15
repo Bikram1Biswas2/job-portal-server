d5ed6eaf6bf13d9aba677c083348a4d7b63c3897
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-15 23:50:19 +0530

    set the origin and credientials

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 const app = express()
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,8 +10,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
-app.use(cors())
+app.use(cors({
+    origin:['http://localhost:5173'],
+    credentials:true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.znhzfas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -38,8 +44,13 @@ async function run() {
     // Auth Related apis
     app.post('/jwt',async(req,res)=>{
       const user = req.body 
-      const token = jwt.sign(user,'secret',{expiresIn: '1h'})
-      res.send(token)
+      const token = jwt.sign(user,process.env.JWT_SECRET,{expiresIn: '1h'})
+      res
+      .cookie('token',token,{
+        httpOnly: true,
+        secure: false,
+      })
+      .send({success: true})
     })
 
     // get all jobs
